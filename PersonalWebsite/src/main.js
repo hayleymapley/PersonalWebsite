bd480c1b00038d15465729fbf938685bc996ab71
Hayley Mapley <40159686+hayleymapley@users.noreply.github.com>
2018-10-08 10:52:45 +1300

    Initial commit
    -Needs optimisation for mobile

---
+++ PersonalWebsite/src/main.js
@@ -0,0 +1,46 @@
+
+//Smooth scrolling using jQuery
+
+$(document).ready(function(){
+
+ 	$("a").on('click', function(event) { 		//Adds smooth scroll to all links
+	    if (this.hash !== "") {					//Ensures that the hash location exists (e.g. #about)...
+      		event.preventDefault();				//...Before overriding default behaviour
+    		var hash = this.hash;				//Stores the hash
+      		$('html, body').animate({			//Uses jQuery's animate() method
+       			 scrollTop: $(hash).offset().top
+      			}, 800
+      		);
+    	}
+  	});
+});
+
+//Slideshow functionality
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("slides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i=0; i < slides.length; i++) {
+		slides[i].style.display ="none";
+	}
+	for (i=0; i< dots.length; i++) {
+		dots[i].className = dots[i].className.replace("active", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+}
+
